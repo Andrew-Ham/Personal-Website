55af5e7e47ae688a475f4449ba0ff15d1a8f42c4
Andrew Ham <yham023@aucklanduni.ac.nz>
2022-11-20 19:16:42 +1300

    Added toastify for contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,11 +1,45 @@
 import "./ContactFormStyles.css"
 import {FaLinkedin} from "react-icons/fa";
 
-import React from 'react'
+import React, {useState} from 'react'
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+
 
 const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  /*
+    toast is from React toastify which allows you to add notifications to app!
+  */
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (!name || !email || !subject || !message) {
+      return toast.error("Please fill out all fields!");
+    }
+    try {
+      setLoading(true);
+      const { data } = await axios.post(`/api/email`, {
+        name, email, subject, message
+      });
+      setLoading(false);
+      toast.success(data.message);
+
+    } catch (err) {
+      setLoading(false);
+      toast.error(
+        err.response && err.response.data.message ? err.response.data.message: err.message
+      )
+    }
+  }
   return (
     <div className = "form">
+      <ToastContainer position = "bottom-center" limit={1} />
       <div className = "contactintro">
         <p>Please connect with me on Linkdein (link to my personal page below)!</p>
         <div className = "linkdein">
@@ -13,16 +47,16 @@ const ContactForm = () => {
         </div>
         <p> Or fill out the form below and I will do my best to respond via email!</p>
         </div>
-        <form>
-            <label>Your name</label>
-            <input type = "text"></input>
+        <form onSubmit={submitHandler}>
+            <label>Your name</label>  
+            <input onChange = {(e) => setName(e.target.value)} type = "text" ></input>
             <label>Email</label>
-            <input type = "email"></input>
+            <input onChange = {(e) => setEmail(e.target.value)} type = "email"></input>
             <label>Subject</label>
-            <input type = "text"></input>
+            <input onChange = {(e) => setSubject(e.target.value)} type = "text"></input>
             <label>Message</label>
-            <textarea row="7" placeholder="Please enter your message here"/>
-            <button className = "btn">Submit</button>
+            <textarea row="7" placeholder="Please enter your message here" onChange = {(e) => setMessage(e.target.value)}/>
+            <button disabled = {loading} type = "submit" className = "btn">{loading ? 'Sending information...' : 'Submit'}</button>
 
             
         </form>
